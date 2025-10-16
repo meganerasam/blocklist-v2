1f5596b370cce77a5849d568a5ce231813d96710
meganerasam <meganerasam@gmail.com>
2025-10-16 10:34:20 +0400

    Update the path of merge_chuncks

--- merge_chunks.php
+++ merge_chunks.php
@@ -5,10 +5,16 @@ $workingAll = [];
 $inactiveAll = [];
 
 for ($i = 1; $i <= $numChunks; $i++) {
-    $w = file(__DIR__ . "/working_domains_chunk_{$i}_result.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    $workingAll = array_merge($workingAll, $w);
-    $in = file(__DIR__ . "/inactive_domains_chunk_{$i}_result.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    $inactiveAll = array_merge($inactiveAll, $in);
+    $wFile = __DIR__ . "/results/retest-results-{$i}/working_domains_chunk_{$i}_result.txt";
+    $inFile = __DIR__ . "/results/retest-results-{$i}/inactive_domains_chunk_{$i}_result.txt";
+    if (file_exists($wFile)) {
+        $w = file($wFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        $workingAll = array_merge($workingAll, $w);
+    }
+    if (file_exists($inFile)) {
+        $in = file($inFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        $inactiveAll = array_merge($inactiveAll, $in);
+    }
 }
 $workingAll = array_unique($workingAll);
 $inactiveAll = array_unique($inactiveAll);
